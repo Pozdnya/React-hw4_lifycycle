0cc390c44bdb6541f2899445443765c76aa63c21
Pozdnya <kika_domination@yahoo.com>
2020-03-02 21:07:55 +0200

    chanhing ClockFC

--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -2,43 +2,6 @@ import React from 'react'
 import style from './style.module.css'
 
 
-//f***ng ts dont work, error ts2339
-// interface State {
-// 	seconds: number,
-// 	interval:NodeJS.Timeout
-// }
-// class Clock extends React.Component<State, {}, any> {
-// 	public constructor(props: any) {
-// 		super(props)
-// 	}
-// 	public state = {
-// 		seconds: 1
-// 	}
-
-
-// 	componentDidMount():any {
-// 		this.componentDidMount.prototype = setInterval(() => this.tick(), 1000)
-
-// 	}
-
-
-// 	componentWillUnmount():void {
-// 		clearInterval(this.componentDidMount.prototype)
-// 	}
-
-// 	tick() {
-// 		this.setState({
-// 			seconds: 1
-// 		});
-// 	}
-// 	shouldComponentUpdate(nextState: State) {
-// 		return nextState !== this.state
-// 	}
-// 	render() {
-// 		return <p className={style.p}>{this.state.seconds}</p>
-// 	}
-// }
-
 const timeCorection = 10800000
 
 export default class Clock extends React.Component {
@@ -46,7 +9,7 @@ export default class Clock extends React.Component {
 		super(props)
 		this.state = {
 			seconds: new Date(0, 0, 0, 0).toLocaleTimeString(),
-			start: new Date()
+			start: Date.now()
 		}
 	}
 
@@ -54,7 +17,6 @@ export default class Clock extends React.Component {
 		console.log(' componentDidMount: компоненты был смонтирован, выполняется после render')
 		this.interval = setInterval(() => this.setState({ seconds: new Date(Date.now() - this.state.start - timeCorection).toLocaleTimeString() }), 1000)
 	}
-
 	//////////////////update
 
 	shouldComponentUpdate(nextState) {
--- src/components/ClockFC/index.jsx
+++ src/components/ClockFC/index.jsx
@@ -2,24 +2,79 @@ import React, { useState, useEffect } from 'react'
 import style from './style.module.css'
 
 const ClockFC = () => {
-	const [date, setDate] = useState(new Date())
-
+	const [time, setTime] = useState(0)
+	const [date, setDate] = useState(Date.now())
+	console.log('render')
 	useEffect(() => {
-		setDate(new Date(0,0,0,0))
-		 setInterval(() => { setDate(new Date(Date.now()-date)) }, 1000)
-		 date.setHours(date.getHours()+3)	
+		console.log(' componentDidMount: компоненты был смонтирован, выполняется после render')
+		const interval = setInterval(() => {
+			setTime(Date.now() - date)
+		}, 1000)
+		return () => {
+			console.log(' componentWillUnmount: компонент будет размонтирован, выполняется в конце жизненного цикла')
+			clearInterval(interval)
+		}
 	}, [])
 
-	useEffect(() => {
-		 clearInterval()
-	}, [date])
-	
-	//console.log(typeof date.toLocaleTimeString())
+	useEffect(() => console.log(' componentDidUpdate: сравнивает state с предыдущим state, не запускается есть shouldComponentUpdate вернет false'), [time])
+
+	const timeCorrection = () => {
+		const timecor = new Date(time)
+		timecor.setHours(timecor.getHours() - 3)
+		return timecor.toLocaleTimeString()
+	}
+
+	///////////////////////////////////
+
+	// import React from 'react'
+
+	// const Timer = () => {
+	//   console.log('Рендер был вызван, потому что произошло обновление')
+	//   const [time, setTime] = React.useState(0)
+	//   const [start] = React.useState(Date.now())
+
+	//   React.useEffect(() => {
+	//     console.log('Монтирование компонента Timer')
+	//     const timer = setInterval(() => setTime(Date.now() - start), 1000)
+	//     return () => {
+	//       console.log('Таймер остановлен и размонтирован')
+	//       clearInterval(timer)
+	//     }
+	//   }, [])
+
+	//   React.useEffect(() => {
+	//     console.log('Произошло обновление time')
+	//   }, [time])
+
+	//   const getTime = () => {
+	//     const formattedTime = new Date(time)
+	//     const date = formattedTime
+	//     date.setHours(date.getHours() - 3)
+	//     return date.toLocaleTimeString()
+	//   }
+
+	//   return (
+	//     <div>
+	//       <h2>{getTime()}</h2>
+	//     </div>
+	//   )
+	// }
+	// export default React.memo(Timer)
+
+
+
+	///////////////////////////////////////////
+	// useEffect(() => {
+	// 	setInterval(() => { setDate(new Date(new Date() - date ).toLocaleTimeString()) }, 1000)
+	// }, [date])
+
+	// useEffect(() => {
+	// 	return () => clearInterval()
+	// }, [])
+
 	return (
-		<p className={style.p}>{date.toLocaleTimeString()}</p>
+		<p className={style.p}>{timeCorrection()}</p>
 	)
 }
-
-//.toLocaleTimeString()
 
 export default React.memo(ClockFC)
